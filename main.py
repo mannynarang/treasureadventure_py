7b7c5a5753fae1791032460864172e76780ab312
mannynarang <mannynarang@gmail.com>
2021-12-26 04:33:49 +0000

    great island escape

---
+++ main.py
@@ -0,0 +1,49 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+move  = input("A skelton is attacking! Lets run away! Which way should we go : Left or Right ").lower()
+
+if(move == "left"):
+    move = input("Great! We need to swim across to the next island. Appears to be a storm coming through. Should we wait or swim? ").lower()
+    if(move !="swim"):
+        move = input("Great! I see few boats. Which one should we board? What color boat should we board? ").lower()
+        if(move =="yellow"):
+            print("Great you escaped! You win")
+        elif(move =="red"):
+            print("Boat caught on fire! You are Dead!!!")
+        elif(move == "blue"):
+            print("A beastly whale broke the boat in half! You drowned!!!")
+        else:
+            print("Dead!! Boat had a leak")
+
+    else:
+        print("Bad Move, Attacked by a trout. You are dead!")
+
+else:
+    print("Bad Move, Another batch of skeltons came. You are dead!")
